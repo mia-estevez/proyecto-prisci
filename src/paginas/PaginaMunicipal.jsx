e44730a9b5e86682249ff52d7a10f195eb09e7f0
Aixa28 <pilar.fuentes2808@gmail.com>
2026-09-18 00:09:01 -0300

    modificando PaginaMunicipal.jsx y subiendo nuevos cambios

--- src/paginas/PaginaMunicipal.jsx
+++ src/paginas/PaginaMunicipal.jsx
@@ -6,16 +6,6 @@ function PaginaMunicipal() {
   return (
     <div className="contenedor-principal">
       
-      {/* 1. Header de Navegación */}
-      <nav className="header-muni">
-        <div className="logo-muni">Neuquén Capital</div>
-        <div className="nav-links">
-          <span>La ciudad</span>
-          <span>Trámites</span>
-          <span>La Muni</span>
-          <span className="link-activo">PRISCI Tools</span>
-        </div>
-      </nav>
 
       {/* 2. Contenedor de Layout Principal */}
       <main className="layout-principal">
@@ -69,14 +59,7 @@ function PaginaMunicipal() {
         </aside>
       </main>
 
-      {/* 3. Pie de Página */}
-      <footer className="footer-muni">
-        <p>Municipalidad de PRISCI | 0800-MUNI-PRISCI | Neuquén | 2026</p>
-        <div>
-          <p>© 2026 - Todos los derechos reservados.</p>
-        </div>
-      </footer>
-
+     
     </div>
   );
 }
