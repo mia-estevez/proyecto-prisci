f80f334e7c15596330c742b0028cc32cd4aaa15a
Aixa28 <pilar.fuentes2808@gmail.com>
2026-09-18 01:55:45 -0300

    creando estructura de notificaciones.jsx

---
+++ src/paginas/notificaciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './notificaciones.css';
+
+
+export default function Notificaciones() {
+  return (
+    <div className="notificaciones-container">
+      <p className="notificaciones-texto">No hay notificaciones aun</p>
+    </div>
+  );
+}
